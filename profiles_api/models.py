6e4271eae5828bcba8935f77523c0fa6362bbbe0
Bose <souparnabose99@gmail.com>
2020-05-16 11:44:10 +0530

    Adding underscore to resolve error

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -14,7 +14,7 @@ class UserProfileManager(BaseUserManager):
         user = self.model(email = email, name = name)
 
         user.set_password(password)
-        user.save(using = self.db)
+        user.save(using = self._db)
 
         return user
 
@@ -23,7 +23,7 @@ class UserProfileManager(BaseUserManager):
 
         user.is_superuser = True
         user.is_staff = True
-        user.save(using = self.db)
+        user.save(using = self._db)
 
         return user
 
