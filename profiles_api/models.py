c1e19a00c52a2c6928c83879ecaffcc97a72659c
Bose <souparnabose99@gmail.com>
2020-05-17 10:02:14 +0530

    Removing sqllite3 & swapping params

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -19,7 +19,7 @@ class UserProfileManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, name , password):
-        user = self.create_user(email, name, password)
+        user = self.create_user(name, email, password)
 
         user.is_superuser = True
         user.is_staff = True
